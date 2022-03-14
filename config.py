daf8e22bc3196e672ae8eb32a50f048fe0aecfe7
pkhromchenko <43190693+pkhromchenko@users.noreply.github.com>
2022-03-14 01:35:00 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# Dict to store access tokens
+twitterAPI_config = {
+  "consumer_key":
+  
+}
+
+# Keywords to define as a giveaway
+search_values = ["giveaway", "nftgiveaway", "to win", "to enter", "sweepstakes", "giveaways"]
+
+toggle_msgs = False
+
+# Triggers for bot actions
+like_triggers = ["like", "favorite"]
+
+dm_triggers = ["dm", "message"]
+
+reply_triggers = ["comment", ]
+
+rt_triggers = ["retweet", "rt", "share"]
+
+follow_triggers = ["follow"]
+
+tag_triggers = ["tag"]
